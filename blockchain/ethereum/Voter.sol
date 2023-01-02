22dc225087480155a28f455ac53b26ea91ec6819
nkemakolam <nkemakolamnnadi3@gmail.com>
2023-01-02 22:26:27 +0100

    adding more solidity code

--- blockchain/ethereum/Voter.sol
+++ blockchain/ethereum/Voter.sol
@@ -2,23 +2,58 @@
 
 pragma solidity ^0.8.0;
 
+
+
 contract Voter {
+
+    struct {
+        uint pos;
+        bool exists;
+    }
+
     uint[] public votes;
     string[] public options;
     mapping (address => bool) hasVoted;
+    mapping (string => OptionPos) posofOption
 
 constructor(string[] memory _options) {
    options = _options;
    votes = new uint[](_options.length);
+    
+    for (uint i = 0; i <options.length; i++);{
+    OptionPos memory option = OptionPos(i, true);
+    posOfOption[options[i]] = option;
+    }
+
 }
 
-function vote(uint option) public {
+  function vote(uint option) public  {
 
     require(option < options.length, "Invalid option");
     require(!hasVoted[msg.sender], "Already voted");
+    recordVote(option);
 
   }
 
+
+function vote(string memory option) public {
+
+    require(!hasVoted[msg.sender], "Already voted");
+
+    for(uint i=0;i < options.length; i++) {
+      string memory currOption = options[i];
+         if (stringEqual(option, currOption)) {
+        recordVote(i);
+        return;
+      }
+    }
+   revert();
+}
+
+
+function stringEqual(string memory a, string memory b) private pure returns(bool){
+    return keccak256(bytes(a)) == keccak256(bytes(b));
+}
   function recordVote(uint option) private{
     hasVoted[msg.sender] = true;
     votes[option] = votes[option] + 1;
