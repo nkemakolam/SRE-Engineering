698610e61c38dcfd6ed4dde30ec1793fd5a19d82
nkemakolam <nkemakolamnnadi3@gmail.com>
2023-01-02 22:37:37 +0100

    adding for Struct code

--- blockchain/ethereum/Voter.sol
+++ blockchain/ethereum/Voter.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 
 contract Voter {
 
-    struct {
+    struct OptionPos {
         uint pos;
         bool exists;
     }
@@ -14,13 +14,13 @@ contract Voter {
     uint[] public votes;
     string[] public options;
     mapping (address => bool) hasVoted;
-    mapping (string => OptionPos) posofOption
+    mapping (string => OptionPos) posOfOption;
 
-constructor(string[] memory _options) {
+  constructor(string[] memory _options) {
    options = _options;
    votes = new uint[](_options.length);
     
-    for (uint i = 0; i <options.length; i++);{
+    for (uint i = 0; i <options.length; i++) {
     OptionPos memory option = OptionPos(i, true);
     posOfOption[options[i]] = option;
     }
@@ -38,22 +38,15 @@ constructor(string[] memory _options) {
 
 function vote(string memory option) public {
 
-    require(!hasVoted[msg.sender], "Already voted");
+require(!hasVoted[msg.sender], "Already voted");
+OptionPos memory optionPos = posOfOption[option];
+require(optionPos.exists, "Invalid option");
+recordVote(optionPos.pos);
 
-    for(uint i=0;i < options.length; i++) {
-      string memory currOption = options[i];
-         if (stringEqual(option, currOption)) {
-        recordVote(i);
-        return;
-      }
-    }
-   revert();
 }
 
 
-function stringEqual(string memory a, string memory b) private pure returns(bool){
-    return keccak256(bytes(a)) == keccak256(bytes(b));
-}
+
   function recordVote(uint option) private{
     hasVoted[msg.sender] = true;
     votes[option] = votes[option] + 1;
